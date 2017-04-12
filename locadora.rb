6585337d0a69ec4b5aa22d2c096f253b259c0610
Carlos Araújo <carlos.araujo@concrete.com.br>
2017-04-12 11:26:36 -0300

    Código da Locadora refatorada para usar as Classes na execução

--- locadora.rb
+++ locadora.rb
@@ -1,4 +1,23 @@
+
+class FilmesEmCatalogo
+  def initialize(id, titulo, genero, valor)
+    @id = id
+    @titulo = titulo
+    @genero = genero
+    @valor = valor
+    descricao()
+  end
+
+  def descricao()
+    puts " ID: #{@id}, #{@titulo}, Genero: #{@genero} R$#{@valor},00 "
+  end
+end
+
 class CarrinhoRegras
+  @@carrinho_qtd = [ ]
+  @@total = 0
+  @@total_de_filmes = 0
+
   def valida_promocao(t)
     if t >= 100 && t < 200
       return 0.1
@@ -20,97 +39,88 @@ class CarrinhoRegras
       return 0
     end
   end
-end
 
-=begin
-class FilmesEmCatalogo
-  def initialize(id, titulo, genero, valor)
-    @id = id
-    @titulo = titulo
-    @genero = genero
-    @valor = valor
-    descricao()
+  def option_carrinho()
+    puts "Deseja incluir um novo filme ?(1 = Sim, 2 = Não): "
+    d = Integer(gets.chomp) #Resposta de Sim ou Não
+    #d = (gets.chomp).to_i #Resposta de Sim ou Não
+    if d == 1
+      inclui_filme_no_carrinho(0)
+    elsif d == 2
+      inclui_filme_no_carrinho(1)
+    else
+      inclui_filme_no_carrinho(1)
+    end
   end
 
-  def descricao()
-    puts " ID: #{@id}, #{@titulo}, Genero: #{@genero} R$#{@valor},00 "
+  def inclui_filme_no_carrinho(option)
+    if option == 0
+      puts "Digite o ID do filme a ser inserido no carrinho: "
+      num_id = Integer(gets.chomp) #ID do filme
+      #num_id = (gets.chomp) #ID do filme
+      @@carrinho_qtd << num_id
+      @@total_de_filmes = @@total_de_filmes + 1
+      temp = somatoria(num_id)
+      @@total += temp
+      option_carrinho()
+    elsif option == 1
+      fecha_carrinho()
+    else
+      fecha_carrinho()
+    end
   end
-end
 
-def option_carrinho()
-  puts "Deseja incluir um novo filme ?(1 = Sim, 2 = Não): "
-  d = Integer(gets.chomp)
-  if d == 1
-    inclui_filme_no_carrinho()
-  elsif d == 2
-    fecha_carrinho()
-  else
-    option_carrinho()
+  def somatoria(num_id)
+    if num_id == 1
+      return 45
+    elsif num_id == 2
+      return 55
+    elsif num_id == 3
+      return 100
+    elsif num_id == 4
+      return 55
+    elsif num_id == 5
+      return 100
+    elsif num_id == 6
+      return 200
+    else
+      return 0
+    end
   end
-end
 
-def inclui_filme_no_carrinho()
-  puts "Digite o ID do filme a ser inserido no carrinho: "
-  num_id = Integer(gets.chomp) #ID do filme
-  $carrinho<<num_id
-  $total_de_filmes = $total_de_filmes + 1
-  somatoria(num_id)
-end
-
-
-def somatoria(num_id) #Fase 2: Implementar um hash de id para valor
-  if num_id == 1
-    $total += 45
-  elsif num_id == 2
-    $total += 55
-  elsif num_id == 3
-    $total += 100
-  elsif num_id == 4
-    $total += 55
-  elsif num_id == 5
-    $total += 100
-  elsif num_id == 6
-    $total += 200
-  else
-    puts "ID invalido!, por favor insira um filme que exista em nosso cadastro. "
-    inclui_filme_no_carrinho()
-  end
-  option_carrinho()
-end
-
-def fecha_carrinho()
-  desconto = valida_promocao($total)
-  temp = $total * desconto
-  total_promocional = $total - temp
-  puts
-  puts "Calculando o valor carrinho..."
-  puts
-  puts "IDs incluidos:  #{$carrinho} "
-  puts
-  puts "Quantidade de filmes alugados: #{$total_de_filmes}"
-  puts
-  puts "Valor total:  R$ #{$total},00"
-  puts
-  puts "Possivel desconto: -#{desconto * 100}%"
-  puts
-  puts "Valor com desconto: R$ #{total_promocional}"
-  puts
-  puts "Aprovar desconto para esse cliente ?(1 = Sim, 2 = Não): "
-  e = Integer(gets.chomp)
-  if e == 1
-    puts ">>> Desconto de #{desconto * 100}% aplicado com sucesso!, total a ser pago: R$ #{total_promocional} <<<"
-  elsif e == 2
-    puts ">>> Compra fechada sem desconto, total a ser pago: R$ #{$total},00 <<<"
-  else
-    desconto = temp = total_promocional = 0
-    fecha_carrinho()
+  def fecha_carrinho()
+    desconto = valida_promocao(@@total)
+    temp = @@total * desconto
+    total_promocional = @@total - temp
+    puts
+    puts "Calculando o valor carrinho..."
+    puts
+    puts "IDs incluidos:  #{@@carrinho_qtd} "
+    puts
+    puts "Quantidade de filmes alugados: #{@@total_de_filmes}"
+    puts
+    puts "Valor total:  R$ #{@@total},00"
+    puts
+    puts "Possivel desconto: -#{desconto * 100}%"
+    puts
+    puts "Valor com desconto: R$ #{total_promocional}"
+    puts
+    puts "Aprovar desconto para esse cliente ?(1 = Sim, 2 = Não): "
+    e = Integer(gets.chomp) #Aprovação do desconto no final da compra
+    #e = (gets.chomp) #Aprovação do desconto no final da compra
+    if e == 1
+      puts ">>> Desconto de #{desconto * 100}% aplicado com sucesso!, total a ser pago: R$ #{total_promocional} <<<"
+    elsif e == 2
+      puts ">>> Compra fechada sem desconto, total a ser pago: R$ #{@@total},00 <<<"
+    else
+      desconto = temp = total_promocional = 0
+      fecha_carrinho()
+    end
   end
 end
 
 #Main **************************************
-$carrinho = [ ]
-$total = 0
-$total_de_filmes = 0
+carrinho = CarrinhoRegras.new()
 
 puts ">> Filmes em catalogo: <<"
 puts
@@ -126,5 +136,4 @@ scp = FilmesEmCatalogo.new(5, "The Scapegoat", "Drama", 100)
 
 bad = FilmesEmCatalogo.new(6, "Meu Malvado Favorito", "Animação", 200)
 
-inclui_filme_no_carrinho()
-=end
+carrinho.inclui_filme_no_carrinho(0)
--- locadora_spec.rb
+++ locadora_spec.rb
@@ -1,5 +1,6 @@
 require_relative "locadora"
 describe CarrinhoRegras do
+
   context "Testes no valor total do Carrinho que deve aplicar uma porcentagem de desconto de acordo com a faixa. Nesse caso o filme de [Ação] não foi adicionado." do
     it "Carrinho com valor >= 100 e < 200 com desconto possível de 10%" do
       obj = CarrinhoRegras.new()
@@ -27,16 +28,62 @@ describe CarrinhoRegras do
       expect(resultado).to eq(0)
     end
   end
-  context "Teste para validar se existe um filme do genero um filme de Açao para a aplicaçao de 5% de desconto" do
-    it "Filme de Açao encontrado" do
+
+  context "Teste para validar se existe um filme do genero um filme de Ação para a aplicação de 5% de desconto" do
+    it "Filme de Ação encontrado" do
       obj = CarrinhoRegras.new()
       desc = obj.valida_genero_acao(true)
       expect(desc).to eq(0.05)
     end
-    it "Filme de Açao nao encontrado" do
+    it "Filme de Ação não encontrado" do
       obj = CarrinhoRegras.new()
       desc = obj.valida_genero_acao(false)
       expect(desc).to eq(0)
+    end
+  end
+
+  context "Somatoria dos ID's" do
+    it "ID 1 tem o valor de 45" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(1)
+      expect(val).to eq(45)
+    end
+    it "ID 2 tem o valor de 55" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(2)
+      expect(val).to eq(55)
+    end
+    it "ID 3 tem o valor de 100" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(3)
+      expect(val).to eq(100)
+    end
+    it "ID 4 tem o valor de 55" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(4)
+      expect(val).to eq(55)
+    end
+    it "ID 5 tem o valor de 100" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(5)
+      expect(val).to eq(100)
+    end
+    it "ID 2 tem o valor de 55" do
+      obj = CarrinhoRegras.new()
+      val = obj.somatoria(6)
+      expect(val).to eq(200)
+    end
+  end
+  context "Caso exista um filme de Ação no carrinho deve ser aplicado mais 5% de deconto" do
+    it "Existe filme de Ação no carrinho" do
+      obj = CarrinhoRegras.new()
+      ac = obj.valida_genero_acao(true)
+      expect(val).to eq(0.5)
+    end
+    it "Existe filme de Ação no carrinho" do
+      obj = CarrinhoRegras.new()
+      ac = obj.valida_genero_acao(false)
+      expect(val).to eq(0)
     end
   end
 end
